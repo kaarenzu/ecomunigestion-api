fe9f9df450b08876666cba8d1b1ffa678022c9be
kaarenzu <karenzcortes0211@gmail.com>
2026-01-20 23:23:22 -0300

    feat: se agrega endpoint health para validar que la api está respondiendo correctamente

--- src/app.js
+++ src/app.js
@@ -6,6 +6,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/health", (req, res) => {
+    res.status(200).json({
+        status: "ok",
+        service: "EcoMuniGestion API",
+        environment: process.env.NODE_ENV
+    });
+});
+
 const reporteRoutes = require('./routes/reporte.routes');
 app.use('/api/reportes', reporteRoutes);
 
